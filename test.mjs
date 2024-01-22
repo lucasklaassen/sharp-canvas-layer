983c0b41bdd70a97d5f614f064295795da2339ba
Lucas Klaassen <lucasklaassen1@gmail.com>
2024-01-21 17:21:15 -0800

    Fix: Remove tests 1.0.0

--- test.mjs
+++
@@ -1,9 +0,0 @@
-import sharp from 'sharp'
-// Generate an image from text
-await sharp({
-  text: {
-    text: 'Hello, world!',
-    width: 400, // max width
-    height: 300 // max height
-  }
-}).toFile('text_bw.png')
